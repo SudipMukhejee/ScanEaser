6b61c9b45efe0c371094cc3ac1e5b6a0cb28fcde
Sudip Mukherjee <sudipmukherjee96144@gmail.com>
2024-04-06 11:15:34 +0530

    routing

---
+++ src/components/feature.jsx
@@ -0,0 +1,13 @@
+// Feature.js
+import React from 'react';
+
+const Feature = () => {
+    return (
+        <div>
+            <h1>Feature Page</h1>
+            <p>Check out our amazing features on this page!</p>
+        </div>
+    );
+};
+
+export default Feature;
