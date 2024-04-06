2e9ac19097dd1de9f7b664ca67044778fdbecaf7
Shreya Chatterjee <chatterjeeshreya523@gmail.com>
2024-04-06 20:48:20 +0530

    Router standard practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import {createBrowserRouter,RouterProvider,createRoutesFromElements,Route} from "react-router-dom"
+import Home from './components/home';
+import About from './components/about';
+import Feature from './components/feature';
+import Layout from './Layout';
 
 function App() {
+
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout/>}>
+        <Route path='' element={<Home/>}/>
+        <Route path='about' element={<About/>}/>
+        <Route path='feature' element={<Feature/>}/>
+      </Route>
+    )
+  )
+
   return (
-    <Navbar/>
+    <RouterProvider router={router}/>
+    
   );
 }
 
 export default App;
+
---
+++ src/components/Signup.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Signup=()=>{
+    return(
+        <div className="text-white">Sign up</div>
+    )
+}
+
+export default Signup
