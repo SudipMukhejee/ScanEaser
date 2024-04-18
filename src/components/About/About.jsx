ef9cbd0880f901e3f74a909d8d37247c0ad29467
SauradipGhosh <sauradip96ghosh@gmail.com>
2024-04-18 10:42:20 +0530

    Sign up Error Fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const About = () => {
   return (
     <div className="text-white w-[80%] mx-auto">
-     
+     hii this is About Page
     </div>
   );
 };
--- src/components/About/ContactForm.jsx
+++ src/components/About/ContactForm.jsx
@@ -1,4 +1,4 @@
-'use client'
+
 
 import React from 'react'
 import { Menu, X, MapPin } from 'lucide-react'
